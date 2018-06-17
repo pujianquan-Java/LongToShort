cda09032eacfabca53fcc41bbc5738b4ae0939b8
pujianquan-Java <40327098+pujianquan-Java@users.noreply.github.com>
2018-06-17 15:10:39 +0800

    sql

---
+++ sql/short.sql
@@ -0,0 +1,35 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.7.17-log : Database - url
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`url` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+
+USE `url`;
+
+/*Table structure for table `longtoshort` */
+
+DROP TABLE IF EXISTS `longtoshort`;
+
+CREATE TABLE `longtoshort` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `long_url` varchar(500) COLLATE utf8_bin DEFAULT NULL,
+  `short_url` varchar(23) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `longtoshort` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
